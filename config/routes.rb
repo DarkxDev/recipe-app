1ca61c851e09dc27458a7279cd19388c294291b3
Mohamed Saleh <mohamed20163858@gmail.com>
2023-05-24 02:03:31 +0300

    Add table to food page

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,6 @@ Rails.application.routes.draw do
 
   root to: 'users#index'
 
-  resources :users
-
-  get '/foods/add', to: 'foods#add', as: 'foods_add'
-  resources :foods
+  delete '/foods/:id', to: 'foods#destroy', as: 'foods_destroy'
 
 end
