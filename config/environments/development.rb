764dcf0b05635c54c58f27ac84f0d8de28389fec
Mohamed Saleh <mohamed20163858@gmail.com>
2023-05-28 19:19:09 +0300

    Try gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,23 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  #test sending email 
+  # config.action_mailer.perform_deliveries = true
+
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   domain: 'https://recipe-app-nr65.onrender.com/', # Replace with your own domain or hostname
+  #   user_name: Rails.application.credentials.dig(:gmail_smtp, :email), # Replace with your Gmail email address
+  #   password: Rails.application.credentials.dig(:gmail_smtp, :password), # Replace with your Gmail password
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true
+  # }
+  # config.action_mailer.raise_delivery_errors = true
+
+  #
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
